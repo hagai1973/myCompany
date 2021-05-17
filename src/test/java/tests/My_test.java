3d8b6b1a739e13b259ff8432a8ca7c74e2b29326
Hagai_Tregerman <Hagai1973@gmail.com>
2021-05-17 20:13:40 +0300

    New project

---
+++ src/test/java/tests/My_test.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class My_test {
+
+
+    @Test
+    public void myTest1(){
+        int myVar = 2;
+        Assertions.assertTrue(1==1);
+    }
+
+    @Test
+    public void myTest2(){
+        int myVar = 2;
+        Assertions.assertTrue(1==1);
+    }
+}
---
+++ src/test/java/tests/Test1Test.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class Test1Test {
+
+
+    @Test
+    public void myTest1(){
+        int myVar = 2;
+        Assertions.assertTrue(1==1);
+    }
+
+    @Test
+    public void myTest2(){
+        int myVar = 2;
+        Assertions.assertTrue(1==1);
+    }
+}
